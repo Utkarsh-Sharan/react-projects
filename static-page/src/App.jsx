b4a712d9695eea47092179b45fafd362db727308
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-12-01 14:53:47 +0530

    create temporary component

--- static-page/src/App.jsx
+++ static-page/src/App.jsx
@@ -1,15 +1,17 @@
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import Challenge from "./components/Challenge";
 
 function App() {
   return (
     <>
+      <Challenge />
       <main className="main">
         <img src={reactLogo} alt="react-logo" width={100} />
 
-        <h1 className="p-10">Fun facts about React!</h1>
+        <h1>Fun facts about React!</h1>
 
-        <ul className="p-10">
+        <ul>
           <li>Was released in 2013</li>
           <li>Was originally created by John Walke</li>
           <li>Has well over 200k stars on GitHub</li>
